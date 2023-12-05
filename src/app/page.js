f642deaf5ca940ce38ce37463dc0332e5f0100f3
Stanley Zhao <stanleyhzhao@gmail.com>
2023-12-05 14:39:11 -0500

    New hero idea

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,20 @@ import Image from "next/image";
 export default function Home() {
   return (
     <section className="flex h-full min-h-screen w-full flex-col items-center justify-center overflow-hidden">
-      <Hero />
+      <section className="flex h-full w-full flex-col items-center justify-center">
+        <div className="absolute inset-0 z-0 overflow-hidden">
+          <div className="jumbo absolute -inset-[10px] opacity-50" />
+        </div>
+        <div className="items-left flex w-full max-w-7xl flex-col px-6 2xl:px-0">
+          <h1 className="z-[1] text-7xl font-semibold tracking-tighter">
+            learn web dev.
+            <br />
+            win cash prizes.
+            <br />
+            all in 4 weeks.
+          </h1>
+        </div>
+      </section>
     </section>
   );
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,6 +2,7 @@
 import { useState, useEffect } from "react";
 import logo from "../../public/logo.svg";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Nav() {
   const [atTop, setAtTop] = useState(true);
@@ -28,9 +29,20 @@ export default function Nav() {
         atTop ? "border-transparent" : "border-neutral-900"
       } bg-transparent px-6 backdrop-blur-lg transition ease-in-out 2xl:px-0`}
     >
-      <div className="flex w-full max-w-7xl items-center gap-3 text-2xl font-semibold">
-        <Image className="h-auto w-8" src={logo} alt="web.lab Logo" />
-        <span>{"{web.lab}"}</span>
+      <div className="flex w-full max-w-7xl items-center">
+        <Link
+          href="/"
+          draggable="false"
+          className="flex items-center gap-3 text-2xl font-semibold"
+        >
+          <Image
+            className="h-auto w-8 select-none"
+            draggable="false"
+            src={logo}
+            alt="web.lab Logo"
+          />
+          {"{web.lab}"}
+        </Link>
       </div>
     </nav>
   );
