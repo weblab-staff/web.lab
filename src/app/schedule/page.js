22cf06ec06ecddb8d9317d39713e23cdce4a446d
Stanley Zhao <stanleyhzhao@gmail.com>
2024-12-21 00:27:37 -0500

    test

--- src/app/schedule/page.js
+++ src/app/schedule/page.js
@@ -1,29 +1,20 @@
 import Rays from "@/components/global/Rays";
-import Faq from "@/components/info/Faq";
 import Schedule from "@/components/info/Schedule";
-export default function Team() {
+
+export default function SchedulePage() {
   return (
     <section className="flex h-full min-h-screen w-full flex-col items-center justify-center overflow-hidden">
       <section className="flex h-full w-full max-w-7xl flex-col items-center justify-center gap-y-8 px-6 py-24 2xl:gap-x-16 2xl:px-0">
         <Rays>
           <section className="flex w-full flex-col items-start gap-2">
             <h1 className="whitespace-pre-wrap text-3xl font-semibold tracking-tighter lg:text-4xl 2xl:text-5xl">
-              rules
+              schedule
             </h1>
             <p className="whitespace-pre-wrap text-base tracking-tighter text-neutral-300 lg:text-lg 2xl:text-xl">
-              full list of rules for the competiton as well as in-depth details
-              about the class can be found here
+              for lectures, workshops, deadlines, and more
             </p>
           </section>
-          <section className="flex w-full flex-col items-start gap-2">
-            <h1 className="whitespace-pre-wrap text-3xl font-semibold tracking-tighter lg:text-4xl 2xl:text-5xl">
-              faq
-            </h1>
-            <p className="whitespace-pre-wrap text-base tracking-tighter text-neutral-300 lg:text-lg 2xl:text-xl">
-              frequently asked questions
-            </p>
-          </section>
-          <Faq />
+          <Schedule />
         </Rays>
       </section>
     </section>
--- src/components/info/Schedule.js
+++ src/components/info/Schedule.js
@@ -25,7 +25,52 @@ const schedule = [
   },
 ];
 
-export default function Schedule() {
+function inferType(event) {
+  const title = event.summary.toLowerCase();
+  if (title.includes("lunch")) return "lunch";
+  if (title.includes("milestone")) return "milestone";
+  if (title.includes("office hours")) return "oh";
+  return "lec";
+}
+
+function createEvent(event) {
+  return {
+    name: event.summary,
+    type: inferType(event),
+    where: event.location,
+    links: event.attachments || [],
+    start: event.start.dateTime,
+    end: event.end.dateTime,
+    lecturers: event.description,
+  };
+}
+
+async function getEvents() {
+  const data = await fetch(
+    "https://www.googleapis.com/calendar/v3/calendars/cc1fa1082653a6746650dce3a7403bfcdd6c898294210579915103e4c450d3e2@group.calendar.google.com/events?key=AIzaSyBWw7tnK1X91Fuv67TqYtc_fRCIo8FWW4Y",
+  );
+  const json = await data.json();
+  const filteredEvents = json.items.filter((event) => {
+    const eventDate = new Date(event.start.dateTime);
+    const now = new Date();
+    const nextYear = now.getFullYear() + 1;
+    const cutoffDate = new Date(nextYear, 0, 1); // January 1st of the following year
+
+    // If it's after February of the current year, set cutoff to January 1st of the year after next
+    if (now.getMonth() === 1 && now.getDate() > 1) {
+      cutoffDate.setFullYear(nextYear + 1);
+    }
+
+    return eventDate >= cutoffDate && event.status !== "cancelled";
+  });
+
+  return filteredEvents;
+}
+
+export default async function Schedule() {
+  const events = await getEvents();
+
+  console.log(events);
   return (
     <div className="w-full rounded-2xl border border-neutral-700 bg-transparent p-4 backdrop-blur-2xl transition duration-300 ease-in-out lg:p-8">
       <h3 className="text-lg font-semibold tracking-tighter text-white lg:text-xl 2xl:text-2xl">
@@ -67,29 +112,26 @@ export default function Schedule() {
           </tr>
         </thead>
         <tbody className="divide-y divide-neutral-700 bg-transparent">
-          {schedule.map((week) => (
-            <tr key={week.title} className="divide-x divide-neutral-700">
+          {events.map((week) => (
+            <tr key={week.name} className="divide-x divide-neutral-700">
               <td className="whitespace-nowrap py-4 pl-4 pr-4 text-sm font-medium text-neutral-300 sm:pl-0">
-                {`${new moment(week.start, "HH:mm:ss").format("h:mm A")} - ${new moment(week.end, "HH:mm:ss").format("h:mm A")}`}
+                {`${moment(week.start.dateTime).format("h:mm A")} - ${moment(week.end.dateTime).format("h:mm A")}`}
               </td>
               <td className="whitespace-nowrap p-4 text-sm text-neutral-300">
-                {week.eventTitle}
-              </td>
-
-              <td className="whitespace-nowrap p-4 text-sm text-neutral-300">
-                {week?.lecturers}
+                {week.name}
               </td>
               <td className="whitespace-nowrap p-4 text-sm text-neutral-300">
-                {week.location}
+                {week.lecturers}
               </td>
-
+              <td className="whitespace-nowrap p-4 text-sm text-neutral-300">
+                {week.where}
+              </td>
               <td className="whitespace-nowrap py-4 pl-4 pr-4 text-sm text-neutral-300 sm:pr-0">
-                {week?.resources &&
-                  week.resources.map((resource) => (
-                    <a href={resource} key={resource}>
-                      {resource}
-                    </a>
-                  ))}
+                {/* {week.resources.map((resource, key) => (
+                  <a href={resource} key={key}>
+                    {resource}
+                  </a>
+                ))} */}
               </td>
             </tr>
           ))}
