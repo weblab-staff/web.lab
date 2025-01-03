768ce420ec930a94dbdf1905c4909e302efa7af4
Stanley Zhao <stanleyhzhao@gmail.com>
2025-01-03 15:37:09 -0500

    Update metadata.js

--- src/data/metadata.js
+++ src/data/metadata.js
@@ -3,7 +3,7 @@ export const metadata = {
   url: "https://weblab.mit.edu/",
   description:
     "At its core, web.lab is a 6-unit MIT IAP class that teaches the fundamentals of web development. It lays the foundations for understanding complicated web technologies and allows enrolled students to take part in a competition to win cash and glory!",
-  preview: "/preview.png",
+  preview: "/img/preview.png",
   piazza: "https://piazza.com/class/m571s5sftuy3gm",
   studentPortal: "https://portal.weblab.is/",
   heroBlurb: "registration is now open!",
