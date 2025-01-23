0a48b35977048c9bd65081863b64939d07f0f989
Stanley Zhao <stanleyhzhao@gmail.com>
2025-01-23 18:55:13 -0500

    Reduce my image bandwidth jeez

--- src/components/team/TeamSection.js
+++ src/components/team/TeamSection.js
@@ -30,24 +30,19 @@ export default function TeamSection({
               )
                 ? "origin-center duration-1000 hover:rotate-180"
                 : "origin-center duration-300 hover:-translate-y-1"
-            } flex h-full w-full flex-col gap-y-2 rounded-2xl border border-neutral-700 bg-transparent p-4 backdrop-blur backdrop-brightness-75 transition-transform ease-in-out [&>div>img]:grayscale [&>div>img]:hover:grayscale-0`}
+            } flex h-full w-full flex-col gap-y-2 rounded-2xl border border-neutral-700 bg-transparent p-4 backdrop-blur backdrop-brightness-75 transition-transform ease-in-out [&>img]:grayscale [&>img]:hover:grayscale-0`}
             key={i}
             style={{ maxWidth: "600px" }}
           >
-            <div className="relative aspect-square h-auto w-full overflow-hidden rounded-md border border-neutral-700">
-              <Image
-                className="object-cover object-center transition duration-300 ease-in-out"
-                fill
-                src={
-                  currentTeam
-                    ? `/img/staff/current/${member?.img || "placeholder1.png"}`
-                    : `/img/staff/alums/${member.year}/${member.img}`
-                }
-                sizes="33vw"
-                quality={50}
-                alt={`web.lab staff ${member.firstName} ${member.lastName}`}
-              />
-            </div>
+            <img
+              className="relative aspect-square h-auto w-full overflow-hidden rounded-md border border-neutral-700 object-cover object-center transition duration-300 ease-in-out"
+              src={
+                currentTeam
+                  ? `/img/staff/current/${member?.img || "placeholder1.png"}`
+                  : `/img/staff/alums/${member.year}/${member.img}`
+              }
+              alt={`web.lab staff ${member.firstName} ${member.lastName}`}
+            />
             <div className="flex w-full flex-col">
               <h2 className="text-lg font-semibold lowercase leading-5 tracking-tighter text-white lg:text-xl 2xl:text-2xl">
                 {member.firstName}{" "}
