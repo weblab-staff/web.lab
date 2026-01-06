99926b199beacedf42aaddf521edeb2e23a47aee
FWJK35 <calvinrodrigue@gmail.com>
2026-01-06 01:02:10 -0500

    updated piazza link

--- src/data/metadata.js
+++ src/data/metadata.js
@@ -4,7 +4,7 @@ export const metadata = {
   description:
     "At its core, web.lab is a 6-unit MIT IAP class that teaches the fundamentals of web development. It lays the foundations for understanding complicated web technologies and allows enrolled students to take part in a competition to win cash and glory!",
   preview: "/img/preview.png",
-  piazza: "https://piazza.com/class/m571s5sftuy3gm",
+  piazza: "https://weblab.is/piazza",
   studentPortal: "https://portal.weblab.is/",
   heroBlurb: "registration is now open!",
   heroBlurbLink: "https://portal.weblab.is/",
